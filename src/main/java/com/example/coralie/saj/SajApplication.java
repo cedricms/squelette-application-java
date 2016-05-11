47f0f77c00cd596a143997a7a40f754a51c0c987
Cédric Maria-Sube <cedric@maria-sube.com>
2016-05-11 07:22:01 +0000

    Initializes spring via Spring Boot.

---
+++ src/main/java/com/example/coralie/saj/SajApplication.java
@@ -0,0 +1,12 @@
+package com.example.coralie.saj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajApplication.class, args);
+	}
+}
